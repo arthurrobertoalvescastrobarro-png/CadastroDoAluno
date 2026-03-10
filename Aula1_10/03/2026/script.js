77f44f91ad60f23aef170a55c97d2ff25602efea
arthurrobertoalvescastrobarro-png <arthur.roberto.alves.castro.barro@gmail.com>
2026-03-10 09:42:11 -0300

    Arthur-Roberto

---
+++ Aula1_10/03/2026/script.js
@@ -0,0 +1,6 @@
+function salvar() {
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    console.log("nome", nome);
+    console.log("idade", idade);
+};
